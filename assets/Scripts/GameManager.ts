1067a2cc0de876ec7610890059d8c19cab5eabd3
sabbirz <sabbir.kuasha@gmail.com>
2024-03-13 08:07:40 +0600

    gravity control

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,26 @@
+import {
+  _decorator,
+  Component,
+  director,
+  PhysicsSystem2D,
+  Node,
+  v2,
+  Vec2,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+  @property
+  gravity: Vec2 = v2(0, 0);
+
+  onLoad() {
+    console.log(PhysicsSystem2D.instance);
+    PhysicsSystem2D.instance.enable = true;
+    // PhysicsSystem2D.instance.gravity = this.gravity;
+  }
+
+  start() {}
+
+  update(deltaTime: number) {}
+}
